050af6a282a87160af4d3d55d1e987f9b545f23c
fmota89 <101104684+fmota89@users.noreply.github.com>
2023-01-04 09:26:29 +0000

    done and working

--- index.js
+++ index.js
@@ -11,9 +11,19 @@ image1.setAttribute('src', randomImageSource); // Sets the first image to the ra
 // Image 2
 let randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
 
-let randomDiceImage2 = 'dice' + randomNumber2 + '.png'; // dice1.png - dice6.png
-let randomImageSource2 = 'images/' + randomDiceImage2; // images/dice1.png - images/dice6.png
+let randomImageSource2 = 'images/dice' + randomNumber2 + '.png'; // images/dice1.png - images/dice6.png
 
-let image2 = document.querySelectorAll('img')[1]; // Selects the second image
-image2.setAttribute('src', randomImageSource2); // Sets the second image to the random dice image
+document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2); // Selects the second image and sets the second image to the random dice image
 
+// If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+// If player 2 wins
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+// If draw
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
